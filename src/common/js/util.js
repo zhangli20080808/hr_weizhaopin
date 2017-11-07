72f26a44366c61bc0fa6c41b32a5134dfd961689
qiya <137971744@qq.com>
2017-11-07 16:22:11 +0800

    qiya

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -4,16 +4,17 @@
  */
 
 
-var user = "http://192.168.5.76:8089/hrm_test"; // 小辉测试本地
-var user = "http://192.168.5.76:8080/hrm"; // 小辉本地
+// var user = "http://192.168.5.76:8089/hrm_test"; // 小辉测试本地
+// var user = "http://192.168.5.76:8080/hrm"; // 小辉本地
 // var user = "http://192.168.4.215:8090/hrm"; // 琦玉本地
 // var user = "http://192.168.4.10:8080/hrm"; // 丸子本地
 // var user = "http://192.168.4.195:8080/hrm"; // 天明本地
 
 // var user="http://121.199.182.2:30004/hrm"
-// var user = "http://192.168.4.157:8080/hrm";  //鹿丸
 
-// var user = "https://aijuhr.com/hrm"; // 正式
+// var user = "http://192.168.4.157:8060/hrm";  //鹿丸
+
+var user = "https://aijuhr.com/hrm"; // 正式
 
 
 exports.ukeyNo = false;//U盾的开关,正式:false,开发:true
