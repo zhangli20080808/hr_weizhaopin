b8943cb5a5c1541398efbaec4f374fd5ec398e36
qiya <137971744@qq.com>
2018-01-20 14:00:50 +0800

    PC简历验证

--- src/components/microresume/add.vue
+++ src/components/microresume/add.vue
@@ -533,7 +533,6 @@
           scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
           success: function (res) {
             var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-            alert(result);
           }
         });
       }
