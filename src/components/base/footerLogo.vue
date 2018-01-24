a90559291198e6f8be47f33ff704498dc1c60d43
yeqiu <yeqiu@iyenei.com>
2018-01-24 20:08:51 +0800

    ###

--- src/components/base/footerLogo.vue
+++ src/components/base/footerLogo.vue
@@ -16,9 +16,8 @@
         align-items: center;
         position: absolute;
         bottom:5px;
-        left:50%;
+        left:135px;
         z-index:2;
-        transform: translate(-52px,-16px);
         height: 48px;        
         text-align: center;
         a{ 
@@ -31,24 +30,26 @@
          }
         }   
       }
-      // .footer_icon {
-      //   display: flex;
-      //   align-items: center;
-      //   position: absolute;
-      //   bottom:5px;
-      //   left:135px;
-      //   z-index:2;
-      //   height: 48px;        
-      //   text-align: center;
-      //   a{ 
-      //    height: 16px;
-      //    .img_detail {
-      //     width: 103px;
-      //     height: 16px;
-      //     background: url(../../common/image/footLogo2.jpg) no-repeat left top;
-      //     background-size: 103px auto;
-      //    }
-      //   }   
-      // }
+    //  .footer_icon {
+    //     display: flex;
+    //     align-items: center;
+    //     position: absolute;
+    //     bottom:5px;
+    //     left:50%;
+    //     z-index:2;
+    //     transform: translate(-52px,-16px);
+    //     height: 48px;        
+    //     text-align: center;
+    //     a{ 
+    //      height: 16px;
+    //      .img_detail {
+    //       width: 103px;
+    //       height: 16px;
+    //       background: url(../../common/image/footLogo2.jpg) no-repeat left top;
+    //       background-size: 103px auto;
+    //      }
+    //     }   
+    //   }
+     
 
 </style>
