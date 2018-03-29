61d9fd94eaf123693e7b59c3a167fc6207e099a9
peien <peien@iyenei.com>
2018-03-29 17:01:59 +0800

    btn

--- src/components/wxIndex/aboutUs.vue
+++ src/components/wxIndex/aboutUs.vue
@@ -361,41 +361,7 @@ import index from 'vue';
         preWorkTeam: [],
         WorkEnvironment: [],
         preCompanyWebsite: [],
-        preCompanyMemorabilia: [
-//           {
-//           description:"国sg",
-//           id:1402,
-//           imageId:13510,
-//           imageUrl:"",
-//           // imageUrl:"http://oyeq6oxdm.bkt.clouddn.com/Upload1512048807341.png",
-          
-//           isActivity:0,
-//           date
-// :
-// "2016z发展历程3"
-//         },
-//         {
-//           description:"国sg",
-//           id:1402,
-//           imageId:13510,
-//           imageUrl:"http://oyeq6oxdm.bkt.clouddn.com/Upload1512048807341.png",
-          
-//           isActivity:0,
-//           date
-// :
-// "2016z发展历程3"
-//         },
-//         {
-//           description:"国sg",
-//           id:1402,
-//           imageId:13510,
-//           imageUrl:"http://oyeq6oxdm.bkt.clouddn.com/Upload1512048807341.png",
-//           isActivity:0,
-//           date
-// :
-// "2016z发展历程3"
-//         }
-        ],
+        preCompanyMemorabilia: [],
         swiperOption: {//以下配置不懂的，可以去swiper官网看api，链接http://www.swiper.com.cn/api/
           // notNextTick是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，<br>　　　　　　　　假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
           notNextTick: true,
@@ -540,7 +506,7 @@ import index from 'vue';
             _this.preCompanyWebsite = res.data.data.CompanyWebsite;
             _this.preWorkTeam = res.data.data.WorkTeam;
             _this.WorkEnvironment = res.data.data.WorkEnvironment;
-            // _this.preCompanyMemorabilia = res.data.data.CompanyMemorabilia
+            _this.preCompanyMemorabilia = res.data.data.CompanyMemorabilia
             _this.companyIntroduction = res.data.data.CompanyWebsite.companyIntroduction
             _this.productIntroductionList = res.data.data.CompanyWebsite.productIntroductionList
           }
@@ -919,7 +885,7 @@ import index from 'vue';
         this.getMainCompanyInfo();
         this.getBranchCompanyInfo();
         if(!this.fansId){
-          // this.userAuthUrl();
+          this.userAuthUrl();
         }else{
           this.getUserInfo();
         }
@@ -987,15 +953,13 @@ p.companyIntroductionTextMore{
   max-height: none;
 }
 .companyIntroductionText{
-  color: #666;
-  font-size: 13px;
-  text-indent: 20px;
-  padding: 20px 0 10px 10px;
-  line-height: 20px;
-  overflow: hidden;
-  transition: all 5s;
-  max-height: 1.8rem;
-  transition: all 5s;
+    color: #666;
+    font-size: 13px;
+    text-indent: 20px;
+    padding: .2rem 0 .1rem .1rem;
+    line-height: .4rem;
+    overflow: hidden;
+    max-height: 1.8rem;
 }
 .arrowBtnWrap{
   display: inline-block;
